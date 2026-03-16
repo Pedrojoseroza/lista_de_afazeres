e43c8bcb6409d651c40734aa6d497e4e407a7617
pedrojoseroza <pedrojs.ol.daroza@gmail.com>
2026-03-16 19:56:49 -0300

    Adição das feats de editar, excluir e concluir tarefas

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,115 @@
 <script setup>
-import { ref, onMounted, watch } from 'vue';
+import { ref, watch } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 const novoTitulo = ref('');
 const novosDetalhes = ref('');
-const antigaLista = JSON.parse(localStorage.getItem('lista') || [{id:1, titulo: "Estudar Programação", detalhes:"asdsadasdasd"}]);
+const posicao = ref(-1);
+const antigaLista = JSON.parse(localStorage.getItem('lista')) || [
+{
+id: 1,
+titulo: 'Tarefa 1',
+detalhes: 'Estudar programação',
+status: 'concluida'
+},
+{
+id: 2,
+titulo: 'Tarefa 2',
+detalhes: 'Estudar física',
+status: 'concluida',
+},
+{
+id: 3,
+titulo: 'Tarefa 3',
+detalhes: 'Treinar xadrez',
+status: 'pendente'
+},
+{
+id: 4,
+titulo: 'Tarefa 4',
+detalhes: 'Jogar futsal',
+status: 'pendente'
+}];
 const listaDeTarefas = ref(antigaLista);
 const novoId = ref(2);
-watch(listaDeTarefas.value, () => {
+watch(listaDeTarefas.value, (novaLista) => {
   console.log(localStorage.getItem('lista'));
-  localStorage.setItem('lista', JSON.stringify(listaDeTarefas.value));
+  localStorage.setItem('lista', JSON.stringify(novaLista));
 })
 function addTarefa() {
-  if (novoTitulo.value.trim() !== '' && novosDetalhes.value.trim()!=="") {
-    listaDeTarefas.value.push({id: novoId, titulo: novoTitulo.value, detalhes: novosDetalhes.value});
-    novoTitulo.value = "";
-    novosDetalhes.value = "";
+if (posicao.value == -1) {
+  if (novoTitulo.value.trim() !== '' && novosDetalhes.value.trim() !== "") {
+    listaDeTarefas.value.push({
+      id: novoId, titulo: novoTitulo.value,
+      detalhes: novosDetalhes.value,
+      status: 'pendente'
+    });
     novoId.value++;
   } else {
     alert("Não deixe nenhum campo vazio ao tentar adicionar tarefas!");
-    novoTitulo.value = "";
-    novosDetalhes.value = "";
+  } 
+} else {
+  const itemAEditar = listaDeTarefas.value[posicao.value];
+  console.log(itemAEditar);
+  itemAEditar.titulo = novoTitulo.value;
+  itemAEditar.detalhes = novosDetalhes.value;
+}
+  novoTitulo.value = "";
+  novosDetalhes.value = "";
+
+}
+function delTarefa(id) {
+  const index = listaDeTarefas.value.findIndex(e => e.id == id);
+  listaDeTarefas.value.splice(index, 1);
+  console.log('Tá entrando' + listaDeTarefas.value[index]);
+};
+function edtTarefa(id) {
+  const index = listaDeTarefas.value.findIndex(e => e.id == id);
+  novoTitulo.value = listaDeTarefas.value[index].titulo;
+  novosDetalhes.value = listaDeTarefas.value[index].detalhes;
+  posicao.value = index;
+}
+function concluirTarefa(id) {
+  const index = listaDeTarefas.value.findIndex(e => e.id == id);
+  if (
+  listaDeTarefas.value[index].status == 'pendente') {
+    listaDeTarefas.value[index].status = 'concluida';
+  } else {
+    listaDeTarefas.value[index].status = 'pendente';
   }
 }
 </script>
 
 <template>
-<section class="container">
-  <div>
-    <h1>
-      Lista de tarefas
-    </h1>
-    <p>
-      Insira o novo título e os detalhes de uma nova tarefa:
-    </p>
-    <input type="text" v-model="novoTitulo" placeholder="título">
-    <input type="text" v-model="novosDetalhes" placeholder="detalhes">
-    <br>
-    <button @click="addTarefa">Adicionar tarefa</button>
-  </div>
-  <div>
-    <ul>
-      <li v-for="tarefa in listaDeTarefas" :key="tarefa.id">
-        {{ tarefa.titulo }}
-      </li>
-    </ul>
-  </div>
-</section>
+  <section class="container">
+    <div class="box">
+      <h1>
+        Lista de tarefas
+      </h1>
+      <p>
+        Insira o novo título e os detalhes de uma nova tarefa:
+      </p>
+      <input type="text" v-model="novoTitulo" placeholder="título">
+      <input type="text" v-model="novosDetalhes" placeholder="detalhes">
+      <br>
+      <button @click="addTarefa">Adicionar tarefa</button>
+    </div>
+    <div class="box">
+      <ul>
+        <li v-for="tarefa in listaDeTarefas" :key="tarefa.id" :class="{concluida: tarefa.status == 'concluida'}">
+          <span @click="concluirTarefa(tarefa.id)">
+            {{ tarefa.titulo }}
+            <span>
+              {{ tarefa.detalhes }}
+            </span>
+          </span>
+          <div>
+            <button @click="edtTarefa(tarefa.id)" ><font-awesome-icon icon="fa-solid fa-pen-to-square"/></button>
+            <button @click="delTarefa(tarefa.id)"> <font-awesome-icon icon="fa-solid fa-trash"/> </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <style scoped>
@@ -54,20 +119,24 @@ function addTarefa() {
   background-color: pink;
   padding: 30px 0 0 0;
 }
-.container div {
-  margin: 0 30vw;
+
+.container div.box {
+  margin: 0 30vw 10px 30vw;
   background-color: plum;
   padding: 20px 15px;
+  border-radius: 10px;
 }
-.container div h1 {
+
+.container div.box h1 {
   text-align: center;
   font-size: 2rem;
   margin-bottom: 5px;
 }
 
-.container div p {
+.container div.box p {
   margin-bottom: 5px;
 }
+
 .container input {
   border: none;
   border-radius: 10px;
@@ -75,5 +144,25 @@ function addTarefa() {
   color: rgb(255, 255, 255);
   padding: 4px;
   margin-bottom: 5px
+}
+
+.container ul li {
+  display: flex;
+  justify-content: space-between;
+}
+.container ul li span.concluida {
+  text-decoration: line-through;
+  font-weight: bold;
+}
+.container ul li span {
+  display: block;
+}
+.container ul li span > span {
+  font-size: 0.8rem;
+  color: rgba(0, 0, 0, 0.596);
+  max-width: 20vw;
+}
+.container ul li.concluida span {
+  text-decoration: line-through;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
+/* add fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
+
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+
+
